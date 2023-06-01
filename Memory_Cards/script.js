bf3b11d811f3ad9bf5579869b4f684c5552a8853
Lucas Tafur <lucastafur@gmail.com>
2023-05-31 23:39:18 -0500

    Funcionando

---
+++ Memory_Cards/script.js
@@ -0,0 +1,104 @@
+// Espera a que la página esté completamente cargada
+document.addEventListener("DOMContentLoaded", (event) => {
+    // Array con todas las tarjetas, cada imagen aparece dos veces
+    const cardsArray = [
+      "images/1.png",
+      "images/1.png",
+      "images/2.png",
+      "images/2.png",
+      "images/3.png",
+      "images/3.png",
+      "images/4.png",
+      "images/4.png",
+      "images/5.png",
+      "images/5.png",
+      "images/6.png",
+      "images/6.png",
+      "images/7.png",
+      "images/7.png",
+      "images/8.png",
+      "images/8.png",
+    ];
+  
+    // Desordena las tarjetas
+    let gameGrid = cardsArray.sort(() => 0.5 - Math.random());
+  
+    // Obtiene el elemento del DOM donde se van a insertar las tarjetas
+    const game = document.querySelector(".memory-game");
+  
+    // Crea cada tarjeta y la agrega al DOM
+    gameGrid.forEach((item) => {
+      const card = document.createElement("div");
+      card.classList.add("memory-card");
+  
+      const cardInner = document.createElement("div");
+      cardInner.classList.add("card");
+  
+      const frontFace = document.createElement("div");
+      frontFace.classList.add("front-face");
+  
+      const backFace = document.createElement("div");
+      backFace.classList.add("back-face");
+      const img = document.createElement("img");
+      img.src = item;
+      backFace.appendChild(img);
+  
+      cardInner.appendChild(frontFace);
+      cardInner.appendChild(backFace);
+      card.appendChild(cardInner);
+  
+      cardInner.dataset.name = item;
+  
+      // Añade un listener de eventos para el clic en la tarjeta
+      cardInner.addEventListener("click", flipCard);
+      game.appendChild(card);
+    });
+  
+    let count = 0; // Para contar cuántas tarjetas se han volteado
+    let firstGuess = ""; // Para almacenar la primera tarjeta volteada
+    let secondGuess = ""; // Para almacenar la segunda tarjeta volteada
+    let previousTarget = null; // Para almacenar la última tarjeta clickeada
+  
+    // Función que se ejecuta cuando se hace clic en una tarjeta
+    function flipCard() {
+      let clicked = this; // Tarjeta clickeada
+      if (clicked === previousTarget || count > 1) {
+        return;
+      }
+  
+      count++;
+      if (count === 1) {
+        firstGuess = clicked.dataset.name;
+        clicked.classList.add("flip");
+      } else {
+        secondGuess = clicked.dataset.name;
+        clicked.classList.add("flip");
+        if (firstGuess === secondGuess) {
+          setTimeout(matchedGuesses, 600); // Si coinciden, llama a la función para manejar la coincidencia
+        } else {
+          setTimeout(resetGuesses, 600); // Si no coinciden, resetea las tarjetas
+        }
+      }
+      previousTarget = clicked;
+    }
+  
+    // Función para resetear las tarjetas no coincidentes
+    function resetGuesses() {
+      firstGuess = "";
+      secondGuess = "";
+      count = 0;
+      var flippedCards = document.querySelectorAll(".flip");
+      flippedCards.forEach((card) => {
+        card.classList.remove("flip");
+      });
+    }
+  
+    // Función para manejar las tarjetas coincidentes
+    function matchedGuesses() {
+      var matchedCards = document.querySelectorAll(".flip");
+      matchedCards.forEach((card) => {
+        card.classList.add("match");
+      });
+      resetGuesses();
+    }
+  });  
